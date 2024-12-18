c8ffe1914e895fd2c08cae6252c38c88f419c83e
vladgibeskul <wgv96300@gmail.com>
2024-12-18 09:22:50 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{/// <summary>
+/// Вывести в столбик 5 раз слово «Привет!» (циклы с условием)
+/// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        public static void Func()
+        {
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine("Привет!!!");
+            }
+        }
+    }
+}
